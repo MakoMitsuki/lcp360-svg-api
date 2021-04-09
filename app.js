dde5ca3b62ef1a94dc8dd42caf17d9f2678b4a84
Mika <makomitsuki@gmail.com>
2021-04-09 11:27:45 -0500

    it works jim

---
+++ app.js
@@ -0,0 +1,17 @@
+const stateJSON = require('./data/stateData.json');
+
+var express = require("express");
+var app = express();
+
+app.get("/fetchData", (req, res, next) => {
+    try {
+        res.json(stateJSON);
+    } catch (e) {
+        console.trace(e);
+        res.sendStatus(500);
+    }
+});
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
